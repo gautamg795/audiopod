7b55a92fe4ba8c8b83125324831083da7ed797a4
Gautam Gupta <blerb795@gmail.com>
2015-04-04 07:14:47 -0700

    working on search

--- static/search.js
+++ static/search.js
@@ -2,31 +2,26 @@
 // 	var responseString = JSON.stringify(response, '', 2);
 // 	document.getElementById('response').innerHTML +=responseString;
 // }
-
-var search = function(){ };
+$("#searchButton").click(function() { search(); });
+var search = function(){ console.log("Attempt to search before API was ready"); };
 	
 
 function onClientLoad(){
+	console.log("onClientLoad called");
 	gapi.client.setApiKey('AIzaSyBO6rT1hTjzEYWmVI2g7IqK_8I_0ZC5J7c');
 	gapi.client.load('youtube', 'v3', function(){
 		search = function () {
-			var toSearch;
-			toSearch = document.getElementById("songtext").value;
-
-			
-			var request = gapi.client.youtube.search.list({
-				q: toSearch , 
-				part: 'snippet'
-			});
-			request.execute();
-				}
-				
-			});
-	// document.getElementById('response').value += '1';
+				var toSearch;
+				toSearch = document.getElementById("songtext").value;
+				var request = gapi.client.youtube.search.list({
+					q: toSearch , 
+					part: 'snippet'
+				});
+				request.execute();
+					}
+				});
+		// document.getElementById('response').value += '1';
 }
-
-
-
 function onSearchResponse(response){
 //we have response as the youtube response
 //we need video id, thumbnail
