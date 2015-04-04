d2a28282728522bad7a8ae5fa081d852fcd20ae5
DylanFlanders <dylan_flanders@yahoo.com>
2015-04-04 14:26:12 -0700

    moved header up on homepage

---
+++ static/homepage.js
@@ -0,0 +1,11 @@
+/*
+ * homepage.js
+ * used to create a new custom url
+ */
+
+ $("#urlButton").click(function() { customizeURL(); });
+
+function customizeURL()
+{
+    var new_id = $("#urlText").val();
+}
