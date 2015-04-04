0ba6aeb4a778381784a933da23fba0b81d1bf7a6
Gautam Gupta <blerb795@gmail.com>
2015-04-04 09:41:19 -0700

    search and stuff

--- static/search.js
+++ static/search.js
@@ -1,8 +1,15 @@
-// function showResponse(response){
-// 	var responseString = JSON.stringify(response, '', 2);
-// 	document.getElementById('response').innerHTML +=responseString;
-// }
+/*
+ * search.js
+ * handles searching and stores results in searchResults var
+ */
+
+var searchResults = [];
 $("#searchButton").click(function() { search(); });
+$('#searchText').bind('keypress', function(e) {
+	if(e.keyCode==13){
+		search();
+	}
+});
 var search = function(){ console.log("Attempt to search before API was ready"); };
 	
 
@@ -11,20 +18,43 @@ function onClientLoad(){
 	gapi.client.setApiKey('AIzaSyBO6rT1hTjzEYWmVI2g7IqK_8I_0ZC5J7c');
 	gapi.client.load('youtube', 'v3', function(){
 		search = function () {
-				var toSearch;
-				toSearch = document.getElementById("songtext").value;
+				var query = $("#searchText").val();
 				var request = gapi.client.youtube.search.list({
-					q: toSearch , 
-					part: 'snippet'
+					q: query, 
+					part: 'snippet',
+					maxResults: 10,
+					type: "video"
 				});
-				request.execute();
+				request.execute(onSearchResponse);
 					}
 				});
 		// document.getElementById('response').value += '1';
 }
 function onSearchResponse(response){
-//we have response as the youtube response
-//we need video id, thumbnail
-document.getElementById('testtext').value= "yup";
-//document.getElementById('testtext').value = response;
+	console.log(response);
+	searchResults = []; // clear old results
+	var resArray = response.result.items;
+	var idx;
+	for (idx = 0; idx < resArray.length; idx++)
+	{
+		var video = new Object();
+		video.vid = resArray[idx].id.videoId;
+		video.title = resArray[idx].snippet.title;
+		video.thumbnail = resArray[idx].snippet.thumbnails.medium.url;
+		video.author = resArray[idx].snippet.channelTitle;
+		video.description = resArray[idx].snippet.description;
+		searchResults.push(video);
+	}
+	if (searchResults.length == 0)
+	{
+		var result = "<div class='alert alert-warning' role='alert'>Sorry, no results were found.</div>";
+		$("#searchResults").empty();
+		$(result).hide().appendTo("#searchResults").fadeIn(1000);
+		return;
+	}
+	var compiledTemplate = _.template($("#searchResultsTemplate").html());
+	var context = {videos : searchResults};
+	$("#searchResults").empty();
+	var result = compiledTemplate(context);
+	$(result).hide().appendTo("#searchResults").fadeIn(1000);
 }
