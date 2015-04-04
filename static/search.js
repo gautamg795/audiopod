77dd363f67e28da3a7654926fb1424d95ceb0a58
Gautam Gupta <blerb795@gmail.com>
2015-04-04 07:07:04 -0700

    make search look better

--- static/search.js
+++ static/search.js
@@ -3,25 +3,25 @@
 // 	document.getElementById('response').innerHTML +=responseString;
 // }
 
-function search(){
-	var toSearch;
-	toSearch = document.getElementById("songtext").value;
-
+var search = function(){ };
 	
-	var request = gapi.client.youtube.search.list({
-		q: toSearch , 
-		part: 'snippet'
-	});
-	request.execute();
-	// document.getElementById('songtext').value = "kappa";
-}
-
 
 function onClientLoad(){
 	gapi.client.setApiKey('AIzaSyBO6rT1hTjzEYWmVI2g7IqK_8I_0ZC5J7c');
 	gapi.client.load('youtube', 'v3', function(){
-		
-	});
+		search = function () {
+			var toSearch;
+			toSearch = document.getElementById("songtext").value;
+
+			
+			var request = gapi.client.youtube.search.list({
+				q: toSearch , 
+				part: 'snippet'
+			});
+			request.execute();
+				}
+				
+			});
 	// document.getElementById('response').value += '1';
 }
 
