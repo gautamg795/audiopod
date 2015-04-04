273f2382ac6d46ab3bcb1853d3f130a37bafb894
Jonathan805 <risingmidnight@ucla.edu>
2015-04-04 08:48:56 -0300

    youtube c client testing

---
+++ static/search.js
@@ -0,0 +1,35 @@
+// function showResponse(response){
+// 	var responseString = JSON.stringify(response, '', 2);
+// 	document.getElementById('response').innerHTML +=responseString;
+// }
+
+function search(){
+	var toSearch;
+	toSearch = document.getElementById("songtext").value;
+
+	
+	var request = gapi.client.youtube.search.list({
+		q: toSearch , 
+		part: 'snippet'
+	});
+	request.execute();
+	// document.getElementById('songtext').value = "kappa";
+}
+
+
+function onClientLoad(){
+	gapi.client.setApiKey('AIzaSyBO6rT1hTjzEYWmVI2g7IqK_8I_0ZC5J7c');
+	gapi.client.load('youtube', 'v3', function(){
+		
+	});
+	// document.getElementById('response').value += '1';
+}
+
+
+
+function onSearchResponse(response){
+//we have response as the youtube response
+//we need video id, thumbnail
+document.getElementById('testtext').value= "yup";
+//document.getElementById('testtext').value = response;
+}
