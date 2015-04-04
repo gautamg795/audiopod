c4eaf9b59665822aa2a46acf5afbed5c477b3c23
Gautam Gupta <blerb795@gmail.com>
2015-04-04 01:52:10 -0700

    more adding template stuff

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/')
 def hello_world():
-    return "Flask works!"
+    return render_template('homepage.html')
 
 
 if __name__ == '__main__':
