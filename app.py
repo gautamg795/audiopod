6f579ac1202af7ec6ab7295229e524c212c70ada
Gautam Gupta <blerb795@gmail.com>
2015-04-04 02:21:37 -0700

    add templates for client/host

--- app.py
+++ app.py
@@ -11,13 +11,12 @@ def hello_world():
 
 @app.route('/client/<room_id>')
 def client(room_id):
-    pass
+    return render_template('client.html', room_id=room_id)
 
 
 @app.route('/host/<room_id>')
 def host(room_id):
-    pass
-
+    return render_template('host.html', room_id=room_id)
 
 if __name__ == '__main__':
         app.run()
