bf9e93d0c744cda68ccd63ec2df1fb0bf04279ad
Jonathan805 <risingmidnight@ucla.edu>
2015-04-05 04:36:00 -0300

    fixed typo

--- static/search.js
+++ static/search.js
@@ -14,14 +14,17 @@ $('#searchText').bind('keypress', function(e) {
 		search();
 	}
 });
+
+// easter egg
 var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
 $(document).keydown(function(e) {
   kkeys.push( e.keyCode );
   if ( kkeys.toString().indexOf( konami ) >= 0 ){
     $(document).unbind('keydown',arguments.callee);
-    // Launch easter egg here          
+   	
   }
 });
+
 var search = function(){ console.log("Attempt to search before API was ready"); };
 	
 
