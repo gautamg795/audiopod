7b4a30a926abb8364af34385e491130cd2090595
Jason Yang <JasonYang96@ucla.edu>
2015-04-04 18:19:18 -0700

    added get_url() implementation

--- mediabox.py
+++ mediabox.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -17,6 +18,15 @@ def client(room_id):
 @app.route('/host/<room_id>')
 def host(room_id):
     return render_template('host.html', room_id=room_id)
+
+def get_url():
+	"returns a adj + noun str"
+	noun_file = "nouns.txt"
+	NOUNS = open(noun_file).read().splitlines()
+	ADJ = open(adj_file).read().splitlines()
+	noun = NOUNS[random.randint(-1,len(NOUNS))]
+	adj = ADJ[random.randint(-1,len(ADJ))]
+	return adj + ' ' + noun
 
 if __name__ == '__main__':
         app.run()
