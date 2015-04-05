64e4451dd19d399a0d86153faeb93c8e06892940
Jonathan805 <risingmidnight@ucla.edu>
2015-04-05 04:33:36 -0300

    konami code added in search.js, no action yet

--- static/search.js
+++ static/search.js
@@ -8,10 +8,19 @@ $(document).ready(function() {
 });
 var searchResults = [];
 $("#searchButton").click(function() { search(); });
+
 $('#searchText').bind('keypress', function(e) {
 	if(e.keyCode==13){
 		search();
 	}
+});
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+$(document).keydown(function(e) {
+  kkeys.push( e.keyCode );
+  if ( kkeys.toString().indexOf( konami ) >= 0 ){
+    $(document).unbind('keydown',arguments.callee);
+    // Launch easter egg here          
+  }
 });
 var search = function(){ console.log("Attempt to search before API was ready"); };
 	
