3172b2d142d91a114f125d6cdd20c6e9eb9965b5
Gautam Gupta <blerb795@gmail.com>
2015-04-08 21:02:53 -0700

    add cache busting for static resources

--- audiopod.py
+++ audiopod.py
@@ -1,12 +1,26 @@
 import random
 from flask import Flask
 from flask import render_template
+from flask import url_for
+import os
 from flask.ext.qrcode import QRcode
 app = Flask(__name__)
 # app.debug = True
 # app.config['SERVER_NAME'] = 'audiopod.me'
 QRcode(app)
 
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 @app.route('/')
 def hello_world():
